93a1086d7385ab373915dab1693e5197e18b0d91
Josh Mandel <jmandel@alum.mit.edu>
2015-03-18 15:56:02 -0700

    Pull changes from GH before regenerating docs

--- provisioning/roles/docs/files/webhook_receiver/update.sh
+++ provisioning/roles/docs/files/webhook_receiver/update.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd ../repo
+git fetch
 for v in $(git for-each-ref refs/remotes/origin --format="%(refname:short)" | 
         cut -d"/" -f2 | 
 	grep -v HEAD && 
 	git tag);
 do
-	git checkout $v;
+	git checkout origin/$v;
 	echo "baseurl: /$v/" > /tmp/_config-$v.yml;
 	jekyll build --source . \
                      --destination ../static/$v \
