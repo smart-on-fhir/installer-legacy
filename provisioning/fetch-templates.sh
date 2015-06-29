1fe4adb56724251a99e23ee695ed82492ce94d67
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2015-06-29 11:19:24 -0400

    Assemble the apps server from compound repos

--- provisioning/fetch-templates.sh
+++ provisioning/fetch-templates.sh
@@ -6,5 +6,5 @@ branch=${2-master}
 # Download configuration templates for SMART Apps
 mkdir $dir
 echo Downloading Configuration Templates
-wget -q https://raw.githubusercontent.com/smart-on-fhir/apps/$branch/static/fhirStarter/apps.json.default -O $dir/apps.json.j2
-wget -q https://raw.githubusercontent.com/smart-on-fhir/apps/$branch/static/fhirStarter/services.js.default -O $dir/services.js.j2
+wget -q https://raw.githubusercontent.com/smart-on-fhir/fhir-starter/$branch/fhirStarter/apps.json.default -O $dir/apps.json.j2
+wget -q https://raw.githubusercontent.com/smart-on-fhir/fhir-starter/$branch/fhirStarter/services.js.default -O $dir/services.js.j2
